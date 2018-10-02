89d5321a318e082f5f3814f4d28a11ad7c72b19c
nwmsno1 <410165862@qq.com>
2018-10-02 16:53:36 +0800

    Create keras_mnist.py

---
+++ keras_mnist.py
@@ -0,0 +1,92 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+
+def train():
+    fashion_mnist = keras.datasets.fashion_mnist
+    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+    train_images = train_images / 255.0
+    test_images = test_images / 255.0
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(28, 28)),
+        keras.layers.Dense(128, activation=tf.nn.relu),
+        keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+    model.compile(optimizer=tf.train.AdamOptimizer(),
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    model.fit(train_images, train_labels, epochs=5)
+    test_loss, test_acc = model.evaluate(test_images, test_labels)
+    print('Test accuracy:', test_acc)
+    return model, test_images, test_labels
+    
+    
+def make_prediction(model, test_images):
+    predictions = model.predict(test_images)
+    print(predictions[0])
+    print(np.argmax(predictions[0]))
+    return predictions
+
+
+def plot_image(i, predictions, true_label, img):
+    predictions, true_label, img = predictions[i], true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                         100 * np.max(predictions),
+                                         class_names[true_label]),
+               color=color)
+
+
+def plot_value_array(i, predictions, true_label):
+    predictions, true_label = predictions[i], true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+
+def main():
+    model, test_images, test_labels = train()
+    predictions = make_prediction(model, test_images)
+    i = 12
+    plt.figure(figsize=(6, 3))
+    plt.subplot(1, 2, 1)
+    plot_image(i, predictions, test_labels, test_images)
+    plt.subplot(1, 2, 2)
+    plot_value_array(i, predictions, test_labels)
+    '''
+    num_rows = 5
+    num_cols = 3
+    num_images = num_rows * num_cols
+    plt.figure(figsize=(2 * 2 * num_cols, 2 * num_rows))
+    for i in range(num_images):
+        plt.subplot(num_rows, 2 * num_cols, 2 * i + 1)
+        plot_image(i, predictions, test_labels, test_images)
+        plt.subplot(num_rows, 2 * num_cols, 2 * i + 2)
+        plot_value_array(i, predictions, test_labels)
+    '''
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
