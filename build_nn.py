135755e2a1dab032106a99e99ef89512ffee11a2
nwmsno1 <410165862@qq.com>
2018-10-04 12:14:32 +0800

    Update build_nn.py

--- build_nn.py
+++ build_nn.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def add_layer(inputs, insize, outsize, activation_function=None):
@@ -33,13 +34,29 @@ def build_nn():
     train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
     init = tf.global_variables_initializer()
-
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.scatter(x_data, y_data)
+    plt.ion()
+    plt.show()
+    
     with tf.Session() as sess:
         sess.run(init)
+        # train
         for i in range(1000):
             sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
+            # see the step improvement
             if i % 50 == 0:
                 print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
+                try:
+                    ax.lines.remove(lines[0])
+                except Exception:
+                    pass
+                prediction_value = sess.run(prediction, feed_dict={xs: x_data})
+                # plot the prediction
+                lines = ax.plot(x_data, prediction_value, 'r-', lw=5)
+                plt.pause(0.5)
 
 
 def main():
